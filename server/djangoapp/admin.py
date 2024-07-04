94e825fafacd7e2906abb4ff18578439c0059961
harshiloza-official <ozaharshil99@email.com>
2024-07-04 12:12:19 -0400

    Module 4 completed updated

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,7 +1,3 @@
-from django.contrib import admin
-#from .models import related models
-
-
 # Register your models here.
 from django.contrib import admin
 from .models import CarMake, CarModel
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from datetime import datetime
-from .restapis import get_request, analyze_review_sentiments, post_review
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -15,7 +14,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
-
+from .restapis import get_request, analyze_review_sentiments, post_review
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -98,17 +97,18 @@ def get_dealerships(request, state="All"):
     return JsonResponse({"status":200,"dealers":dealerships})
     
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-def get_dealer_reviews(request,dealer_id):
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
     if(dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
-        review = get_request(endpoint)
+        reviews = get_request(endpoint)
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
             review_detail['sentiment'] = response['sentiment']
-        return JsonResponse({"Status":200,"reviews":reviews})
+        return JsonResponse({"status":200,"reviews":reviews})
     else:
-        return JsonResponse({"Status":400,"message":"Bad Request"})
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
 def get_dealer_details(request, dealer_id):
